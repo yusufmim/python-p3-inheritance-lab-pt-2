78dede24fbab5ec75234545e5807bb55b55b3bcf
yusufmim <yusufmim2001@gmail.com>
2025-05-29 13:00:37 +0300

    commit v2

---
+++ lib/chatty_student.py
@@ -0,0 +1,11 @@
+class ChattyStudent(Student):
+   def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! "
+              "Oh man, it was so crazy! What, you don't want any spoilers? "
+              "Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            super().raise_hand()
